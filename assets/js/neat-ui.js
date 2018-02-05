27af90324b6a0b5ddcd1d9cd0915df1ff632c9f6
Seth Puckett <puckett.seth@gmail.com>
2018-02-05 14:03:28 -0500

    update neat ui

--- assets/js/neat-ui.js
+++ assets/js/neat-ui.js
@@ -1,7 +1,20 @@
 var NeatUi = (function () {
-  var neatUiGame = new Phaser.Game(NEAT_UI_WIDTH, NEAT_UI_HEIGHT, Phaser.AUTO, 'neat-ui', { preload: preload, create: create, update: update });
 
-  var graphics;
+  var INPUT_GRID_SIZE = NEAT_UI_WIDTH / 3;
+  var INPUT_CELL_SIZE = INPUT_GRID_SIZE / NEAT_INPUT_SIZE;
+
+  var neatUiGame = new Phaser.Game(NEAT_UI_WIDTH, NEAT_UI_HEIGHT, Phaser.AUTO, 'neat-ui', { preload: preload, create: create, update: update });
+  var staticGraphics;
+  var upText;
+  var downText;
+  var leftText;
+  var rightText;
+  var upButton;
+  var downButton;
+  var leftButton;
+  var rightButton;
+
+
 
   function preload() {
 
@@ -10,15 +23,53 @@ var NeatUi = (function () {
   function create() {
     neatUiGame.stage.backgroundColor = "#fff";
 
-    graphics = neatUiGame.add.graphics(0, 0);
+    staticGraphics = neatUiGame.add.graphics(0, 0);
 
-    
+    drawInputGrid();
+    drawButtonOutputs();
   }
 
   function update() {
-    // draw a rectangle
-    graphics.lineStyle(2, 0x0000FF, 1);
-    graphics.drawRect(50, 250, 100, 100);
+    
+  }
+
+  function drawInputGrid() {
+    staticGraphics.lineStyle(1, 0x000, 1);
+
+    for (var i = 0; i < NEAT_INPUT_SIZE; i++) {
+      for (var j = 0; j < NEAT_INPUT_SIZE; j++) {
+        staticGraphics.drawRect(
+          INPUT_CELL_SIZE + INPUT_CELL_SIZE * i, 
+          INPUT_CELL_SIZE + INPUT_CELL_SIZE * j, 
+          INPUT_CELL_SIZE, 
+          INPUT_CELL_SIZE
+        );
+      }
+    }
+  }
+
+  function drawButtonOutputs() {
+    var textX = NEAT_UI_WIDTH - NEAT_UI_WIDTH / 4;
+    var buttonX = textX - INPUT_CELL_SIZE * 5;
+    var buttonSize = INPUT_CELL_SIZE * 2;
+    
+    var yBase = INPUT_CELL_SIZE * 2;
+    var yMod = (INPUT_GRID_SIZE / 4);
+    var buttonYBuffer = 8;
+    var textStyle = { font: 'Bold 18pt Arial', fill: '#000' }
+    var 
+
+
+    upText = neatUiGame.add.text(textX, yBase + yMod * 0, 'Up', textStyle);
+    DownText = neatUiGame.add.text(textX, yBase + yMod * 1, 'Down', textStyle);
+    LeftText = neatUiGame.add.text(textX, yBase + yMod * 2, 'Left', textStyle);
+    RightText = neatUiGame.add.text(textX, yBase + yMod * 3, 'Right', textStyle);
+    
+    staticGraphics.lineStyle(1, 0x000, 1);
+    staticGraphics.drawRect(buttonX, buttonYBuffer + yBase + yMod * 0, buttonSize, buttonSize);
+    staticGraphics.drawRect(buttonX, buttonYBuffer + yBase + yMod * 1, buttonSize, buttonSize);
+    staticGraphics.drawRect(buttonX, buttonYBuffer + yBase + yMod * 2, buttonSize, buttonSize);
+    staticGraphics.drawRect(buttonX, buttonYBuffer + yBase + yMod * 3, buttonSize, buttonSize);
   }
 
   return {
