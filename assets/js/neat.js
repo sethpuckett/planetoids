359e5d312e47cbbdaf527c4631661be6bfaf1790
Seth Puckett <seth.j.puckett@uscis.dhs.gov>
2018-02-07 11:59:44 -0500

    neat updates

--- assets/js/neat.js
+++ assets/js/neat.js
@@ -1,6 +1,7 @@
 var Neat = (function () {
 
   var input;
+  var pool;
 
   var keystate = {
       left: false,
@@ -57,7 +58,70 @@ var Neat = (function () {
       inputY = Math.min(NEAT_INPUT_SIZE - 1, parseInt((adjustedY / SCREEN_SIZE_WITH_BUFFER) * NEAT_INPUT_SIZE));
       input[inputX][inputY] = true;
     });
+  }
 
+  function initializePool() {
+    pool = newPool()
+
+    for (var i = 0; i < POPULATION; i++) {
+      var basic = basicGenome();
+      addToSpecies(basic);
+    }
+
+    initializeRun();
+  }
+
+  // Private Functions
+
+  function basicGenome() {
+    var genome = newGenome();
+    genome.maxNeuron = INPUT_COUNT;
+    mutate(genome);
+    return genome;
+  }
+
+  function addToSpecies() {
+
+  }
+
+  function initializeRun() {
+
+  }
+
+  function newGenome() {
+    return {
+      genes: {},
+      fitness: 0,
+      adjustedFitness: 0,
+      network: {},
+      maxneuron: 0,
+      globalRank: 0,
+      mutationRates: {
+        connections: MUTATE_CONNECTION_CHANCE,
+        link: LINK_MUTATION_CHANCE,
+        bias: BIAS_MUTATION_CHANCE,
+        node: NODE_MUTATION_CHANCE,
+        enable: ENABLE_MUTATION_CHANCE,
+        disable: DISABLE_MUTATION_CHANCE,
+        step: STEP_SIZE
+      }
+    };
+  }
+
+  function mutate(genome) {
+    for (var prop in genome) {
+      if (genome.hasOwnProperty(prop)) {
+        if (getRandomBool()) {
+          genome[prop] = genome[prop] * .95;
+        } else {
+          genome[prop] = genome[prop] * 1.05263;
+        }
+      }
+    }
+  }
+
+  function getRandomBool() {
+    return Math.random() >= 0.5;
   }
 
   return {
